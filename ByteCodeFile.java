30f382ec3952d5f776917e4fe4f158479fe5b74f
Cleyton Oliveira <cleyton.oliveira@match.mt>
2025-03-13 13:06:40 -0300

    feat(Parser)

---
+++ ByteCodeFile.java
@@ -0,0 +1,48 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ByteCodeFile {
+      public static void write(String renderedResult) {             
+            BufferedWriter arqSaida;
+            try {                 
+                  arqSaida = new BufferedWriter(new FileWriter("prog_destino.j"));	
+                  arqSaida.write(".source prog_destino.java\r\n");	
+                  arqSaida.write(".class public prog_destino\r\n");
+                  arqSaida.write(".super java/lang/Object\r\n");
+                  arqSaida.write(".method public <init>()V\r\n");
+                  arqSaida.write(".limit stack 1\r\n");
+                  arqSaida.write(".limit locals 1\r\n");
+                  arqSaida.write("aload_0\r\n");
+                  arqSaida.write("invokespecial java/lang/Object/<init>()V\r\n");
+                  arqSaida.write("return\r\n");
+                  arqSaida.write(".end method\r\n");
+                  arqSaida.write(".method public static main([Ljava/lang/String;)V\r\n");
+                  arqSaida.write(".limit stack 8\r\n");  
+                  arqSaida.write(".limit locals 100\r\n"); /* máximo de variáveis locais (deve ser calculado) */	
+                  
+                  arqSaida.write(renderedResult); /* Aqui que são obtidos os resultados!  */
+		// System.out.println(processaListaComandos(listaComandos));
+	
+                  arqSaida.write("return\r\n");
+                  arqSaida.write(".end method\r\n");
+	
+                  arqSaida.close();
+	
+                  
+            }
+            catch(IOException e) {
+                  System.out.println("Problema no arquivo 'prog_destino.j'");
+            }
+            catch(Exception e) {
+                  System.out.println(e);
+            }
+      }
+}
+
+
+/* 
+Locais para buscar comandos opcode java / comandos bytecode interpretados pela JVM - São comandos de baixo nível/assembler:
+https://javaalmanac.io/bytecode/
+https://en.wikipedia.org/wiki/List_of_Java_bytecode_instructions
+   */
